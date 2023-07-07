b72959d90beb0a33c01ee0682ef23899dea39c8b
aaboiko <aaboiko.exceedteam@gmail.com>
2023-07-08 01:19:54 +0300

    first link model with server

--- web/app/views.py
+++ web/app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
 import json
+import pickle
 
 def index(request):
     html = render_to_string('index.html', {'title': "Text Classifer", 'author': "Anatoliy"})
@@ -11,9 +12,28 @@ def index(request):
 @csrf_exempt
 def getres(request):
     text = request.body
+
+    model = pickle.load(open("../models/model_knn_bin.sav", "rb"))
+    vec = pickle.load(open("../models/vectorizer.sav", "rb"))
+
+    bin = predict_bin(text, model, vec)
     
     obj = {}
-    obj["bin"] = "hjh"
+    obj["bin"] = bin
     obj["num"] = 44
     return HttpResponse(json.dumps(obj), content_type="application/json")
 
+def predict_bin(text, model, vec):
+    text_vec = vec.transform([text])
+
+    bin = "The review is positive"
+    pred = model.predict(text_vec)
+    if pred == 0:
+        bin = "The review is negative"
+
+    return bin
+
+def predict_num(text, model, vec):
+    text_vec = vec.transform(text)
+    pred = model.predict(text_vec)
+    return pred
